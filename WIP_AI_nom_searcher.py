04f871af532bbdfc3fa2b172fc61a347af25a9cb
Ex4m <121984532+Ex4m@users.noreply.github.com>
2023-01-26 17:39:08 +0100

    AI_search-WIP
    Started to work on training and possibility of the machine learning mechanism

--- WIP_AI_nom_searcher.py
+++ WIP_AI_nom_searcher.py
@@ -1,28 +1,16 @@
+import pickle
 import numpy as np
 import pandas as pd
-import tflearn
-from sklearn.preprocessing import LabelEncoder
-from tflearn.data_utils import VocabularyProcessor
-from keras.models import Sequential, load_model
-from keras.layers import Dense
 from keras.preprocessing.text import Tokenizer
+from keras.utils import np_utils
+#from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import load_model
 
-
-
-
-trained_data = pd.read_json("C:/Users/Exa/Desktop/Python/Customs Nomenclature finder/Nom_output", lines = True)
-trained_data = pd.DataFrame(trained_data)
-
-try:
-    loaded_model = load_model("C:/Users/Exa/Desktop/Python/Customs Nomenclature finder/model.tflearn.Nom")
-except Exception as chyba:
-    print(chyba)
-    epochs = int(input("How many epochs to train? "))
-    # Extract input and output sets from data
-    descriptions = trained_data['Description'].values
-    hs_codes = trained_data['HS Code'].values
-
+def train_model(descriptions, hs_codes, epochs):
     # Tokenize descriptions
     tokenizer = Tokenizer(num_words=5000)
     tokenizer.fit_on_texts(descriptions)
@@ -36,11 +24,6 @@ except Exception as chyba:
 
     # Transform the hs_codes into numerical values
     hs_codes = le.transform(hs_codes)
-    #hs_codes = to_categorical(hs_codes)
-
-    print(hs_codes)
-
-
 
     # Build the neural network
     model = Sequential()
@@ -51,14 +34,39 @@ except Exception as chyba:
 
     # Train the model
     model.fit(descriptions, hs_codes, epochs=epochs, batch_size=16)
-    model.save("C:/Users/Exa/Desktop/Python/Customs Nomenclature finder/model.tflearn.Nom")
+    
+    # Save the tokenizer and model
+    with open('tokenizer.pickle', 'wb') as handle:
+        pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    model.save("model.tflearn.Nom")
+    return tokenizer, model
+
+
+trained_data = pd.read_json("Nom_output", lines = True)
+trained_data = pd.DataFrame(trained_data)
+try:
+    loaded_model = load_model("model.tflearn.Nom")
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+except Exception as chyba:
+    print(chyba)
+    epochs = int(input("How many epochs to train? "))
+    # Extract input and output sets from data
+    descriptions = trained_data['Description'].values
+    hs_codes = trained_data['HS Code'].values
+
+    tokenizer, model = train_model(descriptions, hs_codes, epochs)
 
 # Make a prediction
 while True:
     user_input = input("Enter a Description: ")
-    user_input = tokenizer.texts_to_matrix([user_input])
-    hs_code = model.predict(user_input)
-    hs_code = np.argmax(hs_code)
+    if tokenizer:
+        user_input = tokenizer.texts_to_matrix([user_input])
+    else:
+        with open('tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
+        user_input = tokenizer.texts_to_matrix([user_input])
+    hs_code = np.argmax(loaded_model.predict(user_input))
     data_filtered = trained_data.loc[trained_data['HS Code'] == hs_code]
     if data_filtered.empty:
         print("Predicted HS Code is not in the dataset.")
@@ -67,10 +75,7 @@ while True:
         print("Predicted HS Code: ", hs_code)
         print("Predicted Description: ", description)
 
-
-
-
-
+        
 
 
 
